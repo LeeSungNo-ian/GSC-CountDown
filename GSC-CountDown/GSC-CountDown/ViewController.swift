aa5849b8ab5ea43e571a1876b3d76a70cee162e2
LeeSungNo-ian <isnn0103@gmail.com>
2022-09-14 22:44:12 +0900

    feat: 프로젝트 생성

---
+++ GSC-CountDown/GSC-CountDown/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GSC-CountDown
+//
+//  Created by 이성노 on 2022/09/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+}
+
